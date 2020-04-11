6d26664eb1443ef5cb7736155602481ac2c3ef27
ederddsinfo <eder@ddsinfo.com.br>
2020-04-11 00:42:01 -0300

    desafio-backend

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,85 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  
+  return response.status(201).json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  
+  const { title, url, techs } = request.body;
+
+  var repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+  
+  return response.status(201).json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const { id } = request.params;
+
+  const { title, url, techs } = request.body;
+
+  if (!isUuid(id)){
+    return response.status(400).json('Invalid repository ID');
+  }
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id===id);
+
+  if (repositoryIndex < 0){
+    return response.status(400).json('Not found repository with this ID');
+  }
+  
+  repositories[repositoryIndex].title = title;
+  repositories[repositoryIndex].url = url;
+  repositories[repositoryIndex].techs = techs;
+
+  return response.status(200).json(repositories[repositoryIndex]);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const { id } = request.params;
+
+  if (!isUuid(id)){
+    return response.status(400).json('Invalid repository ID');
+  }
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id===id);
+
+  if (repositoryIndex < 0){
+    return response.status(400).json('Not found repository with this ID');
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+
+  const { id } = request.params;
+
+  if (!isUuid(id)){
+    return response.status(400).json('Invalid repository ID');
+  }
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id===id);
+
+  if (repositoryIndex < 0){
+    return response.status(400).json('Not found repository with this ID');
+  }
+
+  repositories[repositoryIndex].likes++;
+
+  return response.status(200).json(repositories[repositoryIndex]);
+  
 });
 
 module.exports = app;
